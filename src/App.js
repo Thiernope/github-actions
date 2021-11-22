52678d5042478caa115d4b76bfd8b5b3e35e567c
thiernope <ntirandth@gmail.com>
2021-11-22 11:41:02 +0200

    qwerty

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Thierry Coder!000!</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <button>OOOOOOO</button>
     </div>
   );
 }
